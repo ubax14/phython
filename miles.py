73377f8e4bbdb17b2a7c087b3a8828b7ebb33d5f
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-08-05 13:02:47 -0700

    Add files via upload

---
+++ miles.py
@@ -0,0 +1,9 @@
+import random
+
+def main():
+    dist_kilm=int(input("enter a number of distance kilometer"))
+    change_d(dist_kilm)
+def change_d(a):
+     miles=a*0.62
+     print("now the convertion is ",miles,"miles")
+main()
